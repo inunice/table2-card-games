323389e9ea5e78b7ff7c39dd1464424a42349b05
inunice <matiaseuni@gmail.com>
2026-01-08 14:26:00 +0800

    feat: filter wip

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -45,12 +45,23 @@ const data: CardGame[] = [
 ]
 
 export default function Home() {
+
+  function isFavorite(current: CardGame) {
+    return current.isFavorite === true;
+  }
+
+  // function filterFavorites =  {
+  //     return data.filter((current) => current.isFavorite === true);
+  // }
+
   return (
     <div className="flex flex-col mx-20 my-12 gap-5">
+
       <div className="flex flex-row justify-center items-center gap-1">
         <Dices size={32} className="text-ocean" />
         <span className="text-3xl font-bold text-coral">Janine's Card Games</span>
       </div>
+
       <div className="grid sm:grid-cols-3 gap-4 grid-cols-2">
       {data.map((cardGame) => (
         <CardGameCard cardGame={cardGame} key={cardGame.id}/>
