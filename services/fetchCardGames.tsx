fd5086b833aa13d9f7a33f669f83e37a1737017b
inunice <matiaseuni@gmail.com>
2026-01-08 14:29:05 +0800

    chore: react build

--- services/fetchCardGames.tsx
+++
@@ -1,9 +0,0 @@
-export async function fetchCardGames(): Promise<CardGame[]> {
-  try {
-    const cardGames: CardGame[] = []; // FETCH HERE
-    return cardGames;
-  } catch (error) {
-    console.error("Failed to fetch card games:", error);
-    return [];
-  }
-}
