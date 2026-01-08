986ffd806d19d95930845ee55c7cb994d1d7b433
inunice <matiaseuni@gmail.com>
2026-01-08 13:18:44 +0800

    feat: styling

--- app/components/CardGameCard.tsx
+++ app/components/CardGameCard.tsx
@@ -4,9 +4,9 @@ interface CardGameProps {
 
 export default function CardGameCard({cardGame}: CardGameProps) {
     return (
-        <div>
-            <span>{cardGame.name}</span>
-            <span>{cardGame.description}</span>
+        <div className="flex flex-col px-4 py-4 bg-muted rounded-xl">
+            <span className="text-lg font-semibold text-wood">{cardGame.name}</span>
+            <span className="text-wood-muted">{cardGame.description}</span>
         </div>
     );
 }
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -13,16 +13,38 @@ const data: CardGame[] = [
         "id": 2,
         "name": "Uno No Mercy",
         "description": "Sad uno"
+    },
+        {
+        "id": 1,
+        "name": "Flip 7",
+        "description": "Six Seven!"
+    },
+    {
+        "id": 2,
+        "name": "Uno No Mercy",
+        "description": "Sad uno"
+    },
+        {
+        "id": 3,
+        "name": "Flip 67",
+        "description": "Six Ten!"
+    },
+    {
+        "id": 4,
+        "name": "Uno With Mercy",
+        "description": "Happy uno"
     }
 ]
 
 export default function Home() {
   return (
-    <div className="">
-      <span>Janine's Card Games</span>
+    <div className="flex flex-col mx-20 my-12 gap-5">
+      <span className="text-3xl font-bold text-coral">Janine's Card Games</span>
+      <div className="grid sm:grid-cols-3 gap-4 grid-cols-2">
       {data.map((cardGame) => (
-        <CardGameCard cardGame={cardGame}/>
+        <CardGameCard cardGame={cardGame} key={cardGame.id}/>
       ))}
+      </div>
     </div>
   );
 }
